8f46515b72309b548efb5d290678b8b759460cc2
Graphicks <51913314+Graphicks@users.noreply.github.com>
2022-03-16 21:47:40 +0000

    - Removed functions from main.py
    - Improved GUI
    
    - Joined the letters when printing what the user did/did not get correct
    
    - Bug Fixes

---
+++ functions.py
@@ -0,0 +1,39 @@
+import random
+
+def generate_word():
+  f = open("word.txt","r")
+  all_words = f.read().splitlines()
+  correct_word = [random.choice(all_words)]
+  return correct_word
+
+
+def checkMatch(correct_word, user_guess, repeat, match_results):
+  if user_guess[0] == correct_word[0]:
+    print(f"\033[1;32;40m{user_guess[0]} you guessed correct!")
+    repeat = False
+    
+
+  else:
+    for i in range(0,len(correct_word[0])):
+      
+      if user_guess[0][i] == correct_word[0][i]:
+        word = (f"\033[1;32;40m{user_guess[0][i]}")
+        match_results.append(word)
+        
+        
+
+      elif user_guess[0][i] in correct_word[0]:
+        word = (f"\033[1;33;40m{user_guess[0][i]}")
+        match_results.append(word)
+        
+  
+      else:
+        word =(f"\033[1;31;40m{user_guess[0][i]}")
+        match_results.append(word)
+        
+    str1 = ''.join(match_results) 
+    match_results.clear()
+    print(str1)
+    
+    
+  return repeat
--- main.py
+++ main.py
@@ -1,56 +1,32 @@
-import random
+from functions import generate_word, checkMatch
+print("\033[1;34;40mWordle 2022")
+
+######### ---------- VARIABLES
+
+correct_word = generate_word()
 
 guesses = 0
-print("Wordle 2022")
-
-
-f = open("word.txt","r")
-all_words = f.read().splitlines()
-
-
-correct_word = [random.choice(all_words)]
-
-
-user_guess = []
 repeat = True
 
-def checkMatch(correct_word, user_guess, repeat):
-  if user_guess[0] == correct_word[0]:
-    print(f"\033[1;32;40m{user_guess[0]} you guessed correct!")
-    repeat = False
-    
-
-  else:
-    for i in range(0,len(correct_word[0])):
-  
-      if user_guess[0][i] == correct_word[0][i]:
-        print(f"\033[1;32;40m{user_guess[0][i]}")
-        
-
-      elif user_guess[0][i] in correct_word[0]:
-        print(f"\033[1;33;40m{user_guess[0][i]}")
-      
-      else:
-        print(f"\033[1;31;40m{user_guess[0][i]}")
-
-  return repeat
+user_guess = []
+match_results = [] #### basically joins all the check match iterations so its not on each different lines, didn't know what to call it tho 
 
 
+
+# --------------- RUN GAME
 while guesses <= 5 and repeat == True:
   
-  guess = input("\033[1;37;40mGuess the word:  ").lower()
-  user_guess.append(guess)
+  guess = input("\033[1;34;40mGuess the word:  \033[1;37;40m").lower()
+  
   
 
   if len(guess) == 5:
     
-    repeat = checkMatch(correct_word, user_guess, repeat)
+    user_guess.append(guess)
+    repeat = checkMatch(correct_word, user_guess, repeat, match_results)
     user_guess.pop(0)
+    
     guesses+=1 
   else:
-    print("Please input a 5 letter word!")
-    
-
-
-  
+    print("\033[1;31;40m\nPlease input a 5 letter word!\n")
   
